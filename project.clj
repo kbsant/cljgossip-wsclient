268e5ca6dd92632f9d10795fd3b203f816482359
kbsant <keansant@gmail.com>
2022-09-04 12:29:16 +0900

    bump deps

--- project.clj
+++ project.clj
@@ -2,8 +2,8 @@
   :description "A default websocket client for cljgossip"
   :url "https://github.com/kbsant/cljgossip-wsclient"
   :dependencies
-  [[org.clojure/clojure "1.10.0"]
-   [org.clojure/data.json "0.2.6"]
-   [medley "1.0.0"]
-   [stylefruits/gniazdo "1.1.1"]]
+  [[org.clojure/clojure "1.11.1"]
+   [org.clojure/data.json "2.4.0"]
+   [medley "1.4.0"]
+   [stylefruits/gniazdo "1.2.1"]]
   :repl-options {:init-ns cljgossip.wsclient})
